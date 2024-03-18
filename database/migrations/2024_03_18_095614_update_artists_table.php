1eecb3646fcaa135211f774e0758397c9dbf2735
leon birch <145602846+leonmeep@users.noreply.github.com>
2024-03-18 10:41:05 +0000

    added new column to table by creating an update migration

---
+++ database/migrations/2024_03_18_095614_update_artists_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('artists', function (Blueprint $table) {
+
+            $table->string('country_of_death', 100)->nullable();
+
+
+
+        });
+
+    }
+
+    public function down(): void
+    {
+        Schema::table('artists', function (Blueprint $table) {
+            $table->string('country_of_death', 100)->nullable();
+        });
+    }
+};
